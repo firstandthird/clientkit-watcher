914bf908eaefd53602d6abaa0009d49e5f888fac
chris <orthagonal1@gmail.com>
2017-01-19 22:35:09 -0600

    unit tests

--- test/test.watcher.js
+++ test/test.watcher.js
@@ -24,3 +24,28 @@ test('will watch a dir and call .process when files are changed', (t) => {
     fs.writeFile('test/watch1/watch1.txt', Math.random(), () => {});
   }, 1000);
 });
+
+test('will watch a dir and ignore specified files', (t) => {
+  const watches = {};
+  watches[`${path.join(__dirname, 'watch1', '*.txt')}`] = 'testTask';
+  const task = new WatcherTask('watcher', {
+    files: watches,
+    debug: true,
+    ignore: 'watch1.txt',
+    delay: 20,
+  }, {
+    runner: {
+      run: (taskName) => {
+        t.equal(false, 'testTask');
+      }
+    }
+  });
+  task.execute({}, {});
+  setTimeout(() => {
+    fs.writeFile('test/watch1/watch1.txt', Math.random(), () => {});
+    setTimeout(() => {
+      t.end();
+      process.exit();
+    }, 2000);
+  }, 1000);
+});
