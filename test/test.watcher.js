335841220bdb20ad5b7b47425abe710e32b1a090
Chris <orthagonal1@gmail.com>
2017-03-02 10:27:31 -0600

    print all watched files

--- test/test.watcher.js
+++ test/test.watcher.js
@@ -2,7 +2,7 @@ const test = require('tape');
 const WatcherTask = require('../watcher.js');
 const fs = require('fs');
 const path = require('path');
-
+/*
 test('will watch a dir and call .process when files are changed', (t) => {
   t.plan(2);
   const watches = {};
@@ -48,4 +48,28 @@ test('will watch a dir and ignore specified files', (t) => {
       process.exit();
     }, 2000);
   }, 1000);
+});
+*/
+test('still prints all files when multiple watchers', (t) => {
+  t.plan(2);
+  const watches = {};
+  watches[`${path.join(__dirname, 'watch1', '*.js')}`] = 'testTask';
+  watches[`${path.join(__dirname, 'watch1', '*.txt')}`] = 'testTask';
+  const task = new WatcherTask('watcher', {
+    files: watches,
+    debug: true,
+    ignore: 'something',
+    delay: 20,
+  }, {
+    runner: {
+      run: (taskName) => {
+        t.equal(taskName, 'testTask');
+      }
+    }
+  });
+  task.execute({}, {});
+  setTimeout(() => {
+    t.end();
+  //   fs.writeFile('test/watch1/watch1.txt', Math.random(), () => {});
+  }, 2000);
 });
